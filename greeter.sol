11a739f2c7e75ffbcd5c6819080fc6e06db99ca8
BrendanTimmons <brendantimmons@live.com.au>
2017-04-14 09:05:58 +1000

    Decided to go back to something a bit simpler.

---
+++ greeter.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.0;
+
+contract mortal {
+    address creator;
+
+    function mortal(){ 
+        creator = msg.sender; 
+    }
+
+    function kill(){ 
+        if (msg.sender == creator) suicide(creator);  
+    }
+}
+
+contract greeter is mortal{
+    string greeting;
+
+    function greeter(string _greeting) public {
+        greeting = _greeting;
+    }
+
+    function greet() constant returns (string){
+        return greeting;
+    }
+
+    //function writeMessage(string _newmessage){
+    //    message = _newmessage;
+    //}
+}
